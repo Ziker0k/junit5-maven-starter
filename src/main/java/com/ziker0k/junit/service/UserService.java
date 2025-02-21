415e27d540c97b4a47eeed66c5c5ec3e19ec56f4
Ziker0k <21022002max@gmail.com>
2025-02-21 07:14:50 +0300

    lesson4 added

--- src/main/java/com/ziker0k/junit/service/UserService.java
+++ src/main/java/com/ziker0k/junit/service/UserService.java
@@ -2,11 +2,18 @@ package com.ziker0k.junit.service;
 
 import com.ziker0k.junit.dto.UserDto;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserService {
+
+    private final List<UserDto> users = new ArrayList<>();
+
     List<UserDto> getAll() {
-        return Collections.emptyList();
+        return users;
+    }
+
+    public boolean add(UserDto userDto) {
+        return users.add(userDto);
     }
 }
--- src/test/java/com/ziker0k/junit/service/UserServiceTest.java
+++ src/test/java/com/ziker0k/junit/service/UserServiceTest.java
@@ -1,16 +1,50 @@
 package com.ziker0k.junit.service;
 
-import org.junit.jupiter.api.Test;
+import com.ziker0k.junit.dto.UserDto;
+import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserServiceTest {
+    private UserService userService;
+
+    @BeforeAll
+    void setUp() {
+        System.out.println("Before all tests: " + this);
+    }
+
+    @BeforeEach
+    void prepare() {
+        System.out.println("Before each test: " + this);
+        userService = new UserService();
+    }
+
     @Test
     void usersEmptyIfNoUserAdded() {
-        var userService = new UserService();
+        System.out.println("Test 1: " + this);
         var users = userService.getAll();
         assertTrue(users.isEmpty());
-//        It's first test
-//        Hi from new branch
+    }
+
+    @Test
+    void userSizeIfUserAdded() {
+        System.out.println("Test 2: " + this);
+        userService.add(new UserDto());
+        userService.add(new UserDto());
+
+        var users = userService.getAll();
+        assertEquals(2, users.size());
+    }
+
+    @AfterEach
+    void cleanUp() {
+        System.out.println("After each test: " + this);
+    }
+
+    @AfterAll
+    void cleanUpAll() {
+        System.out.println("After all tests: " + this);
     }
 }
