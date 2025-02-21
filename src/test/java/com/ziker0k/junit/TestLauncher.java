82c519e5fc0430a93213e12e2fdeab3086b804b7
Ziker0k <21022002max@gmail.com>
2025-02-21 07:46:59 +0300

    lesson5 added

---
+++ src/test/java/com/ziker0k/junit/TestLauncher.java
@@ -0,0 +1,28 @@
+package com.ziker0k.junit;
+
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
+
+public class TestLauncher {
+    public static void main(String[] args) {
+        var launcher = LauncherFactory.create();
+
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+
+        LauncherDiscoveryRequest discoveryRequest = LauncherDiscoveryRequestBuilder
+                .request()
+                .selectors(DiscoverySelectors.selectPackage("com.ziker0k.junit.service"))
+                .build();
+
+        launcher.execute(discoveryRequest, listener);
+
+        try(var printWriter = new PrintWriter(System.out)) {
+            listener.getSummary().printTo(printWriter);
+        }
+    }
+}
